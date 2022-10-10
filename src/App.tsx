0680ed05fc98a3838cdcfb107699ed252ee34959
johnbanyan11 <johnbanyan11@gmail.com>
2022-10-10 10:29:55 +0530

    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import { About } from "./pages/shopping/About";
-import { Home } from "./pages/shopping/Home";
 import { Store } from "./pages/shopping/Store";
 import { Navbar } from "./components/shopping/Navbar";
 import { CartProvider } from "./context/shopping/CartContext";
