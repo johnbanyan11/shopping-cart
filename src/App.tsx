26d40d553bc1f83301c8a985c1c93ed96f3dc1e7
johnbanyan11 <johnbanyan11@gmail.com>
2022-10-10 10:10:17 +0530

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-// import { Route, Routes } from "react-router-dom";
-// import { Container } from "react-bootstrap";
-// import { About } from "./pages/shopping/About";
-// import { Home } from "./pages/shopping/Home";
-// import { Store } from "./pages/shopping/Store";
-// import { Navbar } from "./components/shopping/Navbar";
-// import { CartProvider } from "./context/shopping/CartContext";
-
 import { Home } from "./pages/todolist/Home";
 
 function App() {
-  return (
-    // <CartProvider>
-    //   <Navbar />
-    //   <Container className="mb-4">
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/store" element={<Store />} />
-    //       <Route path="/about" element={<About />} />
-    //     </Routes>
-    //   </Container>
-    // </CartProvider>
-    <Home />
-  );
+  return <Home />;
 }
 
 export default App;
